a15101d28980040ddcdfa79af88fc6ed4c9109b5
b.Mashrapov <b_mashrapov@mail.ru>
2023-02-09 10:32:36 +0100

    CW2.1 correct

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your text without empty lines and paragraphs and press twice \"Enter\": ");
-
         StringBuilder sb = new StringBuilder();
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
@@ -18,31 +15,30 @@ public class Main {
             }
             sb.append(line).append(" ");
         }
+
         String input = sb.toString().trim();
         String[] words = input.split("\\s+");
 
         // Part 1: Count the number of words in the text
-        int wordCount = words.length;
-        System.out.println("Number of words in the text: " + wordCount);
+        System.out.println("Number of words in the text: " + words.length);
 
         // Part 2: Get the top 10 most frequently mentioned words
-        Map<String, Integer> wordFrequency = new HashMap<>();
-        for (String word : words) {
-            wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
-        }
+        Map<String, Long> wordFrequency = Stream.of(words)
+                .map(word -> word.toLowerCase())
+                .collect(HashMap::new,
+                        (m, word) -> m.put(word, m.getOrDefault(word, 0L) + 1L),
+                        HashMap::putAll);
 
-        List<Map.Entry<String, Integer>> entries = new ArrayList<>(wordFrequency.entrySet());
-        Collections.sort(entries, (a, b) -> {
-            int compare = b.getValue().compareTo(a.getValue());
-            if (compare == 0) {
-                return a.getKey().compareTo(b.getKey());
-            }
-            return compare;
-        });
-
-        System.out.println("Top 10 most frequently mentioned words: ");
-        for (int i = 0; i < 10 && i < entries.size(); i++) {
-            System.out.println(entries.get(i).getKey() + ": " + entries.get(i).getValue());
-        }
+        wordFrequency.entrySet().stream()
+                .sorted((a, b) -> {
+                    int compare = b.getValue().compareTo(a.getValue());
+                    if (compare == 0) {
+                        return a.getKey().compareTo(b.getKey());
+                    }
+                    return compare;
+                })
+                .limit(10)
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
     }
+
 }
